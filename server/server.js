4aed377b183949e4703876d60b6d3e8504ee1eeb
Aniketh <anikethalla@gmail.com>
2026-02-20 23:27:16 +0530

    setup done0

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+
